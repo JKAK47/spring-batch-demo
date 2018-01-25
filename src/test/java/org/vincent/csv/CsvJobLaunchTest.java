6a3f4007b650dd3f2ffa45553f25d0a06691f976
JKAK47 <JKAK47@users.noreply.github.com>
2018-01-25 19:13:03 +0800

    pptx 修改
    pptx 修改

---
+++ src/test/java/org/vincent/csv/CsvJobLaunchTest.java
@@ -0,0 +1,42 @@
+package org.vincent.csv;
+
+import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * csv 数据处理job测试类
+ * @author pengrong
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:demo-csv/spring/Application.xml"})
+public class CsvJobLaunchTest extends AbstractJUnit4SpringContextTests{
+
+	@Autowired
+	private JobLauncher joblauncher;
+	@Resource(name="csvjob")
+	private Job job;
+	@Test
+	public void testCsvJob() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		JobExecution execution =joblauncher.run(job, new  JobParametersBuilder().toJobParameters());
+		System.out.println("Exit Status : " + execution.getStatus());
+		Assert.assertEquals(ExitStatus.COMPLETED, execution.getExitStatus());
+	}
+
+}
