070de6c40fbf989fb50901bcce24528e6bcfd439
PR <1557862201@qq.com>
2018-07-10 07:51:42 +0800

    java ThreadLocal

--- src/main/java/org/vincent/threadlocal/TestNum.java
+++ src/main/java/org/vincent/threadlocal/TestNum.java
@@ -1,22 +1,35 @@
 package org.vincent.threadlocal;
 
+import org.apache.http.annotation.GuardedBy;
+import org.apache.http.annotation.ThreadSafe;
+
+@ThreadSafe
 public class TestNum {
 	 // ①通过匿名内部类覆盖ThreadLocal的initialValue()方法，指定初始值  
     private static ThreadLocal<Integer> seqNum = new ThreadLocal<Integer>() {
-    	//覆盖ThreadLocal Value 默认初始值
-    	@Override
-        public Integer initialValue() {  
-            return 0;  
-        }  
-    };  
-  
-    // ②获取下一个序列值  
-    public Integer getNextNum() {  
-        seqNum.set(seqNum.get() + 1);  
-        return seqNum.get();  
-    }  
-  
-    public static void main(String[] args) {  
+       //覆盖ThreadLocal Value 默认初始值
+       @Override
+       public Integer initialValue() {
+           return 0;
+       }
+   };
+
+    /**
+     * 这样value就是线程安全的了
+     */
+    @GuardedBy("this")
+    private  int value;
+    public synchronized int getValue() {
+        return value;
+    }
+
+    // ②获取下一个序列值
+    public Integer getNextNum() {
+        seqNum.set(seqNum.get() + 1);
+        return seqNum.get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         TestNum sn = new TestNum();  
         // ③ 3个线程共享sn，各自产生序列号  
         TestClient t1 = new TestClient(sn);  
@@ -24,16 +37,23 @@ public class TestNum {
         TestClient t3 = new TestClient(sn);  
         t1.start();  
         t2.start();  
-        t3.start();  
-    }  
-  
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+    }
+
+    /**
+     * 操作 TestNum 类的 seqNum ThreadLocal 类型变量 子线程
+     */
     private static class TestClient extends Thread {  
         private TestNum sn;  
   
         public TestClient(TestNum sn) {  
             this.sn = sn;  
         }  
-  
+
+        @Override
         public void run() {  
             for (int i = 0; i < 3; i++) {  
                 // ④每个线程打出3个序列值  
--- src/main/java/org/vincent/threadlocal/ThreadLocalTest.java
+++ src/main/java/org/vincent/threadlocal/ThreadLocalTest.java
@@ -1,45 +1,52 @@
 package org.vincent.threadlocal;
 
+/**
+ * 新建子线程和main线程，对比两个线程往threadLocal 变量里面插入数据后是否能分别获取到数据
+ */
 public class ThreadLocalTest {
-	ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
-    ThreadLocal<String> stringLocal = new ThreadLocal<String>();
- 
-     
-    public void set() {
-        longLocal.set(Thread.currentThread().getId());
-        stringLocal.set(Thread.currentThread().getName());
-    }
-     
-    public Long getLong() {
-    	Long lon=longLocal.get();
-        return lon;
-    }
-     
-    public String getString() {
-        return stringLocal.get();
-    }
-     
-    public static void main(String[] args) throws InterruptedException {
-        final ThreadLocalTest test = new ThreadLocalTest();
-         
-         
-        //test.set();
-        System.out.println(test.getLong());
-        System.out.println(test.getString());
-     
-         
-        Thread thread1 = new Thread(){
-            public void run() {
-                test.set();
-                //获取thread-01线程的线程私有变量
-                System.out.println(test.getLong());
-                System.out.println(test.getString());
-            };
-        };
-        thread1.start();
-        thread1.join();
-        //获取main线程 
-        System.out.println(test.getLong());
-        System.out.println(test.getString());
-    }
+		ThreadLocal<Long> longLocal = new ThreadLocal<>();
+		ThreadLocal<String> stringLocal = new ThreadLocal<>();
+
+		public ThreadLocal<Long> getLongLocal() {
+				return longLocal;
+		}
+
+		public void set() {
+				longLocal.set(Thread.currentThread().getId());
+				stringLocal.set(Thread.currentThread().getName());
+		}
+
+		public Long getLong() {
+				Long lon = longLocal.get();
+				return lon;
+		}
+
+		public String getString() {
+				return stringLocal.get();
+		}
+
+		public static void main(String[] args) throws InterruptedException {
+				final ThreadLocalTest test = new ThreadLocalTest();
+
+				//main 线程去set 值到threadLocal
+				test.set();
+				//main 线程去获取 long 和 String
+				System.out.println(test.getLong());
+				System.out.println(test.getString());
+
+
+				Thread thread1 = new Thread(() -> {
+						test.set();
+						//获取thread-01线程的线程私有变量
+						System.out.println("long = " + test.getLong());
+						System.out.println("string = " + test.getString());
+				});
+				thread1.start();
+				thread1.join();
+				//获取main线程set 到threadLocal的值
+				//先删除 main线程往LongLocal 这个threadlocal 插入的数据
+				test.getLongLocal().remove();
+				System.out.println(test.getLong());
+				System.out.println(test.getString());
+		}
 }
